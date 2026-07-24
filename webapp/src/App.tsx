dd21609baad91e66cb9962be858ad01d09e18bae
Anton Shevchenko <shevchenko-anton2014@yandex.ru>
2026-07-24 16:06:56 +0300

    feat(webapp): enable agentation in development

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,7 +1,13 @@
 import { RouterProvider } from '@tanstack/react-router'
+import { Agentation } from 'agentation'
 
 import { router } from './routes'
 
 export default function App() {
-  return <RouterProvider router={router} />
+  return (
+    <>
+      <RouterProvider router={router} />
+      {import.meta.env.DEV && <Agentation />}
+    </>
+  )
 }
